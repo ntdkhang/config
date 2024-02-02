fd6406e9ebde4dae8260b28d472ae1b577c56914
ntdkhang <ntdkhang143@gmail.com>
2024-02-02 13:21:23 -0600

    setup lsp for ios

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,13 +2,25 @@ local lsp_zero = require('lsp-zero')
 local util = require("lspconfig.util")
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-lsp_zero.on_attach(
-function(client, bufnr)
+lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
     lsp_zero.default_keymaps({buffer = bufnr})
-end
-)
+end)
+
+require('lspconfig').sourcekit.setup({
+    capabilities = capabilities,
+    cmd = {
+        "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp",
+    },
+    root_dir = function(filename, _)
+        return util.root_pattern("buildServer.json")(filename)
+        or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
+        or util.find_git_ancestor(filename)
+        or util.root_pattern("Package.swift")(filename)
+    end,
+})
+
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
@@ -33,7 +45,7 @@ require('mason-lspconfig').setup({
 lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "gd", vim.lsp.buf.declaration, opts)
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
   vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
   vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
@@ -46,19 +58,6 @@ lsp_zero.on_attach(function(client, bufnr)
 end)
 
 
-require('lspconfig').sourcekit.setup({
-    capabilities = capabilities,
-    cmd = {
-        "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp",
-    },
-    root_dir = function(filename, _)
-        return util.root_pattern("buildServer.json")(filename)
-        or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
-        or util.find_git_ancestor(filename)
-        or util.root_pattern("Package.swift")(filename)
-    end,
-})
-
 
 local cmp = require('cmp')
 local cmp_format = lsp_zero.cmp_format()
@@ -67,6 +66,53 @@ cmp.setup({
     preselect = 'none',
     completion = {
         completeopt = 'menu,menuone,noinsert,noselect'
+    },
+    formatting = { -- copy from [https://github.com/hrsh7th/nvim-cmp/discussions/609#discussioncomment-5727678]
+        fields = { "abbr", "menu", "kind" },
+        format = function(entry, item)
+            -- Define menu shorthand for different completion sources.
+            local menu_icon = {
+                nvim_lsp = "LSP",
+                nvim_lua = "LUA",
+                luasnip  = "SNIP",
+                buffer   = "BUFF",
+                path     = "PATH",
+            }
+            -- Set the menu "icon" to the shorthand for each completion source.
+            item.menu = menu_icon[entry.source.name]
+
+            -- Set the fixed width of the completion menu to 60 characters.
+            -- fixed_width = 20
+
+            -- Set 'fixed_width' to false if not provided.
+            local fixed_width =  false
+
+            -- Get the completion entry text shown in the completion window.
+            local content = item.abbr
+
+            -- Set the fixed completion window width.
+            if fixed_width then
+                vim.o.pumwidth = fixed_width
+            end
+
+            -- Get the width of the current window.
+            local win_width = vim.api.nvim_win_get_width(0)
+
+            -- Set the max content width based on either: 'fixed_width'
+            -- or a percentage of the window width, in this case 20%.
+            -- We subtract 10 from 'fixed_width' to leave room for 'kind' fields.
+            local max_content_width = fixed_width and fixed_width - 10 or math.floor(win_width * 0.3)
+
+            -- Truncate the completion entry text if it's longer than the
+            -- max content width. We subtract 3 from the max content width
+            -- to account for the "..." that will be appended to it.
+            if #content > max_content_width then
+                item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 3) .. "..."
+            else
+                item.abbr = content .. (" "):rep(max_content_width - #content)
+            end
+            return item
+        end,
     },
     sources = {
         {name = 'nvim_lsp'},
@@ -93,7 +139,12 @@ cmp.setup({
                 if cmp.visible() then
                     cmp.select_next_item()
                 else
-                    fallback()
+                    local status_ok, luasnip = pcall(require, "luasnip")
+                    if status_ok and luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
                 end
             end, { "i", "s" }
         ),
@@ -102,7 +153,12 @@ cmp.setup({
                 if cmp.visible() then
                     cmp.select_prev_item()
                 else
-                    fallback()
+                    local status_ok, luasnip = pcall(require, "luasnip")
+                    if status_ok and luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
                 end
             end, { "i", "s" }
         ),
